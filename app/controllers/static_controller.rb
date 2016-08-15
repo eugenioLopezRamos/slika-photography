06ab3b0da5c60e11a1834b1162e46b1a5e1d1159
e0plus <eugenionlopez@gmail.com>
2016-08-15 00:29:49 +0000

    site display ok - prev to ajax impl

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def admin
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/admin'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,12 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'static#home'
+  
+  get '/admin',  to: 'static#admin'
+  
+  resources :people, :models, :urban, :events, only: [:show]
+  
+  
 end
