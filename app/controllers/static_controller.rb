dcd37947110dfecab155bed14ae867a840425f1d
e0plus <eugenionlopez@gmail.com>
2016-08-17 16:41:47 +0000

    Implements AJAX, JS needs adjs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,59 @@
 class StaticController < ApplicationController
   def home
-  end
-  
-  def show
+    respond_to do |format|
+    format.html {render 'home'}
+    end
   end
 
-  def admin
+  def show
+    respond_to do |format|
+    format.html {render 'home'}
+    format.js
+    end
   end
+  
+  def show_models
+    respond_to do |format|
+    format.html {render 'home'}
+    format.js
+    end
+  end
+  
+  def show_urban
+    respond_to do |format|
+    format.html {render 'home'}
+    format.js
+    end
+  end
+  
+  def show_events
+    
+  end
+  
+  def show_contact
+    
+  end
+
+  def show_blog
+    @tab = params[:tab]
+    respond_to do | format |
+    format.html {render 'home', locals: {tab: @tab}}
+    end
+  end
+  
+  
+  def show_404
+
+  end
+  
+  
+
+
+
+
+  def admin
+    
+
+  end
+  
 end
