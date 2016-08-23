7513280e7f50ad306ba839728784ef7f7273e6e0
e0plus <eugenionlopez@gmail.com>
2016-08-23 20:24:36 +0000

    fixes fb perf issues via timeouts

--- app/controllers/contact_mailer_controller.rb
+++ app/controllers/contact_mailer_controller.rb
@@ -6,14 +6,7 @@ class ContactMailerController < ApplicationController
     subject = params[:clientSubject]
     message = params[:clientMessage]
     SiteMailer.new_contact_email(name, email, subject, message).deliver_now
-    
-    #respond_to do |format|
-    #format.js {redirect_to '/contact'} #{ render :partial => 'layouts/jumbotron', :locals => {:tab => "homeTab"} }
-    #end
-    #render :js => 'alert("We have been contacted");'
     end
-    
-    #<%= render :partial => 'users', :collection => @users, :locals => {:size => 30} %>
 
 end
 
