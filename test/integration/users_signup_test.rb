a03d29a414f46c0601cd13ee959d76972891537f
e0plus <eugenionlopez@gmail.com>
2016-08-25 01:44:39 +0000

    signup polish

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,4 +4,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test "valid creation info" do
+    get new_admin_user_path
+    assert_difference 'User.count', 1 do
+      post admin_users_path, params: { admin_user: {name: "Example User",
+                                              email: "user@example.com",
+                                              password: "password",
+                                              password_confirmation: "password"} }
+      end
+      follow_redirect!
+      assert_template 'users/show'
+  end
+  
+  
 end
