da4bc4beceb291bbbd8d3b9bdf0a89c241864a68
e0plus <eugenionlopez@gmail.com>
2016-08-26 01:40:55 +0000

    basic post tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :content, presence: true
+  
+  
+  
+  
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,26 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @user = users(:michael)
+    @post = @user.posts.build(content: "lorem ipsum")
+  end
+  
+  test "post should be valid" do
+    assert @post.valid?
+  end
+  
+  test "post should have an user_id" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+  
+  test "content should be present" do
+    @post.content = "      "
+    assert_not @post.valid?
+  end
+  
+  
+  
 end
