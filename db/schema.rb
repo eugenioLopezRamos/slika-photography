a5149837833d11b0430118d4b1e759f02e79c6f9
e0plus <eugenionlopez@gmail.com>
2016-08-26 01:02:04 +0000

    migrates blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825140415) do
+ActiveRecord::Schema.define(version: 20160826005903) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
