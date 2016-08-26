756325618c733d8a0ec5cdb80e0428b0c92964d7
e0plus <eugenionlopez@gmail.com>
2016-08-26 19:20:35 +0000

    adds post creation + sanitization

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -6,12 +6,23 @@ class Admin::PostsController < ApplicationController
     end
     
     def create
-        
-        
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            flash[:success] = "Post successfully created"
+            redirect_to admin_user_url(current_user)
+        else
+            render 'new'
+        end
     end
     
     def show
         @post = Post.find(params[:id])
     end
     
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :logged_in_admin_user
   
   def new
-      @user = User.new
+    @user = User.new
   end
   
   def show
