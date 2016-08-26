9aa618e9b2e659c86d3353242c3054ad501f9a8a
e0plus <eugenionlopez@gmail.com>
2016-08-26 15:11:44 +0000

    improves user controller and tests

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,37 +1,24 @@
 class Admin::UsersController < ApplicationController
+  before_action :logged_in_admin_user
   
-
   def new
-    if logged_in? && admin_user
-    @user = User.new
-    else
-    flash[:danger] = "You are not allowed to create new users"
-    redirect_to admin_user_path(current_user)
-    end
+
+      @user = User.new
   end
   
   def show
-    if logged_in?
     @user = User.find(params[:id])
-    else
-      redirect_to admin_login_path
-      flash.now[:danger] = "Please login to access this section"
-    end
   end
   
   
   def create
-    
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to admin_user_url(@user)
-      flash[:success] = "Welcome!"
-
+      redirect_to admin_user_url(current_user)
+      flash[:success] = "User successfully created"
     else
-     render 'new'
+      render 'new'
     end
-    
   end
   
   private
@@ -42,7 +29,20 @@ class Admin::UsersController < ApplicationController
     
     # Confirms an admin user.
     def admin_user
-     current_user.admin?
+      current_user.admin?
+    end
+    
+    #confirms user being logged in AND being an admin user
+    def logged_in_admin_user 
+      unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to admin_login_path #and return
+      end
+      
+      if logged_in? && !admin_user
+        flash[:danger] = "You are not an admin"
+        redirect_to admin_user_path(current_user)
+      end
     end
     
 end
