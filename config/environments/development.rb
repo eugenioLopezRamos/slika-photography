33efa483241826bcf6fd7d7af0681fb35b85333c
e0plus <eugenionlopez@gmail.com>
2016-08-30 14:41:29 +0000

    adds post deletion

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   #host = 'rails-implementation-e0plus.c9users.io'
   #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
-
+  #
 
 ### ACTIVATE MAIL HERE
 
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -7,7 +7,7 @@ class PostTest < ActiveSupport::TestCase
   
   def setup
     @user = users(:michael)
-    @post = @user.posts.build(content: "lorem ipsum")
+    @post = @user.posts.build(title: "Hello world!", content: "lorem ipsum")
   end
   
   test "post should be valid" do
@@ -30,10 +30,23 @@ class PostTest < ActiveSupport::TestCase
   
   test "associated posts should be destroyed when user is deleted" do
     @user.save
-    @user.posts.create!(content: "loren ipsum")
+    @user.posts.create!(title: "hello world", content: "loren ipsum")
     assert_difference 'Post.count', -1 do
       @user.destroy
     end
+  end
+  
+  test "an user should be able to delete its own posts" do
+    @post = @user.posts.create!(title: "hello world!", content: "lorem ipsum")
+    get admin_user_path(@user)
+    assert_select 'div.userName'
+    assert_select 'a.post-delete'
+    
+    assert_difference Post.count, -1 do
+      delete admin_post_path(@post)
+    end
+    assert_redirected_to admin_user_path(@user)
+    
   end
   
   
