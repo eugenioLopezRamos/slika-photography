0e7b64a01f6b3ed6c497a8ecb40ff9d31d0c01b0
e0plus <eugenionlopez@gmail.com>
2016-08-31 03:00:56 +0000

    adds user edit integration test

---
+++ test/integration/user_edit_test_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserEditTestTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "admin should be able to delete users" do
+    
+  end
+  
+  
+end
