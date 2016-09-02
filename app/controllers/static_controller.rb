9e1f12b15af2b28f87db5b8c2d6f926067ed7239
e0plus <eugenionlopez@gmail.com>
2016-09-02 23:49:57 +0000

    should be last pushstate commit - blog linking from nav menu clicks working correcttly

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -19,15 +19,22 @@ class StaticController < ApplicationController
   end
   
   def retrieve_tabs
-    if params[:id] == "undefined" || params[:id].nil?
-      @id = "1"
+    if params[:tab] != "blogTab" then
+      if params[:id] == "undefined" || params[:id].nil?
+        @id = "1"
       else 
         @id = params[:id]
-    end
+      end
+
     
-    if params[:post_id].nil? then @post = Post.last #need to fix this
-      else @post = Post.find(params[:post_id])
+    elsif params[:tab] == "blogTab"
+      if params[:id] == "undefined" || params[:id].nil?
+        @post = Post.last
+      else
+        @post = Post.find(params[:id])
+      end
     end
+ 
 
     render partial: 'layouts/jumbotron' 
     
