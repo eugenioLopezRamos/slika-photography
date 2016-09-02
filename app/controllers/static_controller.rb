7368241e37c6cd35af1a185e2eed7c046eda1e77
e0plus <eugenionlopez@gmail.com>
2016-09-02 15:06:23 +0000

    fixes minor presentation bugs wrt blog pushstate - no functionality change

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,11 +7,14 @@ class StaticController < ApplicationController
     respond_to do |format|
     format.html {render 'home'}
     format.js {@parameters = params}
+    format.json 
     end
   end
   
   def retrieve_posts
-    @post = params[:post_id]
+    if params[:post_id] == "last" then @post= Post.last 
+      else @post = Post.find(params[:post_id])
+    end
     render partial: 'admin/posts/post'
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true
   scope :by_date_desc, -> { order(created_at: :desc) } #It seems using default scopes is...complicated to say at least, even for this case, which I thought it'd be fine,
   #eg. http://weblog.jamisbuck.org/2015/9/19/default-scopes-anti-pattern.html, http://stackoverflow.com/questions/25087336/why-is-using-the-rails-default-scope-often-recommend-against
   #
-  
-  
+
   
   
 end
