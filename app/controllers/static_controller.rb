41a3d59cf41e5252803ea1023fa5123fe554ecc8
eugenio <eugenionlopez@gmail.com>
2016-09-06 14:58:35 -0300

    need to refactor wheel and mousemove to make them compat and DRY up

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,43 @@ class StaticController < ApplicationController
    redirect_to '/home'
   end
 
+
+  
+
+  def show
+    respond_to do |format|
+    format.html {render 'home'}
+    format.js
+    end
+  end
+
+  
+  def retrieve_posts
+    if params[:slug] == "last" then @post= Post.last 
+      else @post = Post.find_by(slug: params[:slug])
+    end
+    render partial: 'admin/posts/post'
+  end
+
+  
+  def retrieve_tabs
+    if params[:tab] != "blogTab" then
+      if params[:id] == "undefined" || params[:id].nil?
+        @id = "1"
+      else 
+        @id = params[:id]
+      end 
+    elsif params[:tab] == "blogTab"
+      if params[:id] == "undefined" || params[:id].nil?
+        @post = Post.last
+      else
+        @post = Post.find_by(slug: params[:id])
+      end
+    end
+    render partial: 'layouts/jumbotron'   
+  end
+
+
   def redirect
     @tab = request.env['PATH_INFO'].downcase!
     begin
@@ -11,44 +48,9 @@ class StaticController < ApplicationController
       redirect_to "/404" and return
     end   
   end
-  
 
-  def show
-    respond_to do |format|
-    format.html {render 'home'}
-    format.js
 
-    end
-  end
-  
-  def retrieve_posts
-    if params[:slug] == "last" then @post= Post.last 
-      else @post = Post.find_by(slug: params[:slug])
-    end
-    render partial: 'admin/posts/post'
-  end
-  
-  def retrieve_tabs
-    if params[:tab] != "blogTab" then
-      if params[:id] == "undefined" || params[:id].nil?
-        @id = "1"
-      else 
-        @id = params[:id]
-      end
 
-    
-    elsif params[:tab] == "blogTab"
-      if params[:id] == "undefined" || params[:id].nil?
-        @post = Post.last
-      else
-        @post = Post.find_by(slug: params[:id])
-      end
-    end
- 
-
-    render partial: 'layouts/jumbotron' 
-    
-  end
 
   def show_404
 
