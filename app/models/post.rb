e0c243f7397814c7eafc5bd1315874543b05e16a
eugenio <eugenionlopez@gmail.com>
2016-09-12 15:03:06 -0300

    fixes small error with error messages on new password reset page

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,11 @@ class Post < ApplicationRecord
   before_validation :create_slug
   validates :slug, presence: true
 
-  scope :by_date_desc, -> { order(created_at: "'desc'") } #It seems using default scopes is...complicated to say at least, even for this case, which I thought it'd be fine,
+  scope :by_date_desc, -> { order(created_at: "desc") } #It seems using default scopes is...complicated to say at least, even for this case, which I thought it'd be fine,
   #eg. http://weblog.jamisbuck.org/2015/9/19/default-scopes-anti-pattern.html, http://stackoverflow.com/questions/25087336/why-is-using-the-rails-default-scope-often-recommend-against
   # another option, if this were a bigger database, would be to use find_by, which is ALWAYS ascending & faster since
   #it works with batches, and then just presenting the posts in the view with CSS flex-direction: column-reverse
-  scope :by_date_asc, -> {order(created_at: "'asc'")}
+  scope :by_date_asc, -> {order(created_at: "asc")}
 
   def to_param
   	slug
