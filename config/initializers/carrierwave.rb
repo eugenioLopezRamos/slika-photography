b007f3cc7afd838955e90205f22bee1a7056af68
eugenio <eugenionlopez@gmail.com>
2016-09-14 17:11:01 -0300

    Adds comma to separate fog parameters

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,8 +18,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
 
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID']            # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']     # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
     :region                 => ENV['AWS_S3_BUCKET']               # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'lap-files'               # required
