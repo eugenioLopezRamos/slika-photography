4857cbee3f6f078b215167cb7b50adeae82a2c73
eugenio <eugenionlopez@gmail.com>
2016-09-14 17:46:47 -0300

    Passes last post redirect as param instead of listing as interp string in route

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,9 +20,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                             # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],            # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
-    :region                 => ENV['AWS_S3_BUCKET']               # optional, defaults to 'us-east-1'
+    :region                 => ENV['AWS_S3_REGION']               # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'lap-files'               # required
+  config.fog_directory  = ENV['AWS_S3_BUCKET']               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   config.fog_public     =  true                               # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
