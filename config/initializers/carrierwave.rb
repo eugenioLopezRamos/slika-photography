9f4f35aba263d31627110a0b726fb7aa8fe6c7fa
eugenio <eugenionlopez@gmail.com>
2016-09-16 12:43:12 -0300

    Allows directory style display of items in S3 bbucket in upload/show.html.erb(basic version)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,8 @@ CarrierWave.configure do |config|
     end
   end
 
+
+
   config.fog_credentials = {
 
     :provider               => 'AWS',                             # required
@@ -26,4 +28,5 @@ CarrierWave.configure do |config|
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   config.fog_public     =  true                               # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  
 end
