e073d863b1a676e1e6cb924c2cf6ccc91599336d
eugenio <eugenionlopez@gmail.com>
2016-09-22 09:31:18 -0300

    Adds attribute accessor for auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :reset_token, :activation_token
+    attr_accessor :remember_token, :reset_token, :activation_token, :auth_token
     
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
