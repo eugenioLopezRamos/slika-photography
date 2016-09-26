1afd686c74a41e63f147ee1197a4e89775b6fc9a
eugenio <eugenionlopez@gmail.com>
2016-09-26 18:24:32 -0300

    Begins adding tests for the admin controller

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -4,4 +4,22 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  #Most of the tests for the admin controller are integration tests, since they require interaction between the view, the backend and S3
+
+
+  test "User is correctly redirected to admin_login_path when requesting /admin/" do
+    get '/admin'
+    assert_redirected_to admin_login_path
+    follow_redirect!
+    assert_template 'admin/sessions/new'
+  end
+
+  test "User is shown the upload show page when requesting it" do
+    get '/admin/upload'
+    assert_template 'admin/upload/upload_show'
+
+  end
+
+
 end
---
+++ test/integration/admin_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class AdminControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+ test "Files uploaded from the file manager are correctly sent to S3" do
+
+
+ end
+
+ test "Files deleted through the file manager view are correctly removed from the S3 bucket" do
+
+
+ end
+
+ test "Files downloaded from S3 through the file manager are correctly sent to the client" do
+
+ end
+
+
+
+end
