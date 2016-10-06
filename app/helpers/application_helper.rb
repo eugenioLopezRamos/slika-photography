54bc1c4914e3d1a3e67b58f26dde1917ec7b622d
eugenio <eugenionlopez@gmail.com>
2016-10-05 22:46:46 -0300

    See README.md TODO list for changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-
-    size_breakpoints = [2000, 1500, 1100, 900, 700, 480, 100] #used on the file manager to convert uploads and on slidersContent
-    #to know what image size options are available
     
     def full_title(page_title = '')
         base_title = "Leonardo Antonio PhotoArt"
