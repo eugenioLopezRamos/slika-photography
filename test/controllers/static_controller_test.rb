0d76ed0e5305381134f8cefa717ddb8fd52cdc3c
eugenio <eugenionlopez@gmail.com>
2016-10-11 17:13:24 -0300

    Adds static controller tests (checks if slides tabs are generated correctly)

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -17,8 +17,44 @@ class StaticControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "redirect to lowercase url in /:id's too" do
+
   	get '/bLoG/A-NEW-FANTaSTIC-POST'
   	assert_redirected_to '/blog/a-new-fantastic-post'
+
   end
+
+  test "each tab with slides should successfully show all images in its corresponding folder" do
+    
+    @tabs = ApplicationHelper::image_tabs.map {|tab| tab.gsub('Tab', '')}
+  
+    @tabs.each do |tab|
+      get "/#{tab}"
+
+      @public = assigns(:public_folder)
+      @assets = assigns(:assets_dir)
+      @image_dir = assigns(:image_dir)
+    
+      images = Dir.entries("#{Rails.root}/#{@public}#{@assets}#{@image_dir}/") - %w(. ..)
+   
+      img_count = images.select {|filename| filename.include? "original-"}.length
+      img_ids = images.select {|filename| filename.include? "original-"}
+
+      img_ids.map! do |id|
+        id = id.gsub!("original-", "/")
+        "#{@assets}#{@image_dir}#{id}"
+      end
+
+      assert_select ".#{tab}Slide", img_count
+
+      img_ids.each do |id|
+        id.gsub!(/\/+/, "\\/") #Escapes the forwards slashes
+        id.gsub!(/\.+/, "\\.") #Escapes any dots
+
+        assert_select "##{id}", 1  
+      end
+    end
+  end
+
+
 
 end
