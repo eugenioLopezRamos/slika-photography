024c3397a003769bc1e1d54a3a441797351c5e9a
eugenio <eugenionlopez@gmail.com>
2016-10-11 18:08:09 -0300

    Adds data-attribute testing for static controller slides tab testing

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -33,24 +33,46 @@ class StaticControllerTest < ActionDispatch::IntegrationTest
       @public = assigns(:public_folder)
       @assets = assigns(:assets_dir)
       @image_dir = assigns(:image_dir)
+      @sizes_per_file = assigns(:sizes_per_file)
+      @base_file = assigns(:base_file)
     
       images = Dir.entries("#{Rails.root}/#{@public}#{@assets}#{@image_dir}/") - %w(. ..)
    
       img_count = images.select {|filename| filename.include? "original-"}.length
       img_ids = images.select {|filename| filename.include? "original-"}
+      img_full_routes = img_ids
 
-      img_ids.map! do |id|
+      img_full_routes.map! do |id|
         id = id.gsub!("original-", "/")
         "#{@assets}#{@image_dir}#{id}"
       end
 
       assert_select ".#{tab}Slide", img_count
 
-      img_ids.each do |id|
+      img_full_routes.each do |id|
+
+        filename = String.new
+       # debugger
+        img_ids.each do |name|
+          if name.include? id
+            filename = name.split("/")[-1]
+          end
+        end
+        correct_route = id.gsub(filename, '')
+        
+        correct_sizes = @sizes_per_file[filename]
+      
         id.gsub!(/\/+/, "\\/") #Escapes the forwards slashes
         id.gsub!(/\.+/, "\\.") #Escapes any dots
 
-        assert_select "##{id}", 1  
+        assert_select "##{id}", 1
+        assert_select "##{id}" do
+  
+          assert_select "[data-sizes]", {:count => 1, :value => "#{correct_sizes}"}
+          assert_select "[data-file]", {:count => 1, :value => "#{filename}"}
+          assert_select "[data-route]", {:count => 1, :value => "#{correct_route}"}          
+        end
+      #  assert_select "data-sizes",   
       end
     end
   end
