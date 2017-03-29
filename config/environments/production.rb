94b8b4e57da23ed9bbe3176b93e22afa3ae37f41
Eugenio <eugenionlopez@gmail.com>
2017-03-29 12:39:01 -0300

    reenable ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
 end
